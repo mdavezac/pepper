241704e5523cecb33ad6e520e52ab8dffd5c86a9
David Pérez-Suárez <dps.helio@gmail.com>
2018-01-14 15:00:30 +0000

    Update upstream git repo and DRY-fied setup run

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,32 +4,35 @@ set -e
 prefix=$(pwd)
 condiment_dir=$prefix/CondimentStation
 virtenv=$prefix/build/salt-env
-condiment_repo=git@github.com:UCL-RITS/CondimentStation.git
+condiment_repo=git@github.com:mdavezac/CondimentStation.git
 
 mkdir -p $(pwd)/build
 
+## If the CondimentStation hasn't been run before, create the virtenv
 if [ ! -d "$virtenv" ]; then
   python3 -m venv $virtenv
   . $virtenv/bin/activate
   pip install --upgrade pip salt click GitPython mako pytest ipython virtualenv packaging
 fi
 
+## If running this on a mac, install Homebrew!!
 if [[ "$(uname)" == "Darwin" ]] && [[ ! -e /usr/local/bin/brew ]]
 then
    sudo chown -R $(whoami) /usr/local
    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+## If the Condiment pinch.py doesn't exists...  then clone repository!
 if [[ ! -e "$condiment_dir/bin/pinch.py" ]] ; then
   git clone $condiment_repo
 fi
 
-$virtenv/bin/python $condiment_dir/bin/pinch.py setup server_hierarchy $prefix
-$virtenv/bin/python $condiment_dir/bin/pinch.py setup syspath $prefix
-$virtenv/bin/python $condiment_dir/bin/pinch.py setup minion $prefix
-$virtenv/bin/python $condiment_dir/bin/pinch.py setup pillar $prefix
-$virtenv/bin/python $condiment_dir/bin/pinch.py setup sync
+setups=(server_hierarchy syspath minion pillar sync)
+for setup_i in ${setups[@]}; do
+    $virtenv/bin/python $condiment_dir/bin/pinch.py setup ${setup_i} $prefix
+done
 
+## If a black-garlic repository exists, then update the states
 if [[ -d "$prefix/black-garlic/.git" ]] ; then
   $virtenv/bin/python $condiment_dir/bin/pinch.py update
 fi
